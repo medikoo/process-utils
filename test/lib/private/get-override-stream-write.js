431862f13b8643a1d02c6aec75ffe9edd8b5cb34
Mariusz Nowak <medyk@medikoo.com>
2018-09-19 13:25:22 +0200

    refactor: disambiguate returned 'write' function

--- test/lib/private/get-override-stream-write.js
+++ test/lib/private/get-override-stream-write.js
@@ -7,7 +7,7 @@ module.exports = (type, overrideStream) =>
 		const stream = process[`std${ type }`];
 		const { write } = stream;
 		let data, passedWrite;
-		const { write: boundWrite, originalWrite, restoreWrite } = overrideStream(
+		const { superWrite, originalWrite, restoreWrite } = overrideStream(
 			(chunk, writeToOriginal) => {
 				data = chunk;
 				passedWrite = writeToOriginal;
@@ -20,7 +20,7 @@ module.exports = (type, overrideStream) =>
 		t.notEqual(write, overridenWrite, "Should override write");
 		t.equal(originalWrite, write, "Should expose overriden method");
 		t.equal(data, "Foo\nBar", "Should override with passed function");
-		t.equal(passedWrite, boundWrite, "Should pass direct write function");
+		t.equal(passedWrite, superWrite, "Should pass direct write function");
 		t.equal(stream.write, write, "Should restore original write");
 		t.end();
 	});
