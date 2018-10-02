6b94dc796f6b66a5b96167e585f8a71ec5795091
Mariusz Nowak <medyk@medikoo.com>
2018-10-02 10:21:01 +0200

    fix: ensure set and defined properties are stringified
    Fixes #1

--- override-env.js
+++ override-env.js
@@ -1,7 +1,21 @@
 "use strict";
 
+const ensureObject = require("es5-ext/object/valid-object");
+
 module.exports = () => {
 	const cache = process.env;
-	process.env = {};
+	process.env = new Proxy(
+		{},
+		{
+			defineProperty(target, key, inputDescriptor) {
+				return Object.defineProperty(target, key, {
+					configurable: true,
+					enumerable: true,
+					value: `${ ensureObject(inputDescriptor).value }`,
+					writable: true
+				});
+			}
+		}
+	);
 	return { originalEnv: cache, restoreEnv: () => (process.env = cache) };
 };
--- test/override-env.js
+++ test/override-env.js
@@ -10,6 +10,14 @@ test("overrideEnv", t => {
 	t.notEqual(process.env, env, "Should override env");
 	t.equal(process.env.FOO, undefined, "Should not copy existing variables");
 	t.equal(originalEnv, env, "Should expose original env at original env property");
+	process.env.marko = null;
+	t.equal(process.env.marko, "null", "Should automatically stringify set properties");
+	Object.defineProperty(process.env, "hola", { value: 34 });
+	t.deepEqual(
+		Object.getOwnPropertyDescriptor(process.env, "hola"),
+		{ configurable: true, enumerable: true, value: "34", writable: true },
+		"Should override property definitions"
+	);
 	restoreEnv();
 	t.equal(process.env, env, "Should restore original env after calling a callback");
 	delete env.FOO;
