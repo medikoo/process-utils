f12192c837842cc64916feabf9a606afbc1db882
Mariusz Nowak <mariusz@medikoo.com>
2019-10-21 15:02:54 +0200

    test: Split overrideEnv tests

--- test/override-env/index.js
+++ test/override-env/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const test        = require("tape")
-    , overrideEnv = require("../override-env");
+    , overrideEnv = require("../../override-env");
 
 test("overrideEnv", t => {
 	const { env } = process;
@@ -23,39 +23,6 @@ test("overrideEnv", t => {
 			);
 			restoreEnv();
 			t.equal(process.env, env, "Should restore original env after calling a callback");
-		} finally {
-			delete env.FOO;
-		}
-		t.end();
-	});
-
-	t.test("Whitelist", t => {
-		env.FOO = "bar";
-		env.LOREM = "ispum";
-		try {
-			const { restoreEnv } = overrideEnv({ whitelist: ["FOO"] });
-			t.equal(process.env.FOO, "bar", "Should expose whitelisted variables");
-			t.equal(process.env.LOREM, undefined, "Should not expose not whitelisted variables");
-			process.env.BAR = "elo";
-			t.equal(env.BAR, undefined, "Should not leak new vers on cached env");
-
-			restoreEnv();
-		} finally {
-			delete env.FOO;
-			delete env.LOREM;
-		}
-		t.end();
-	});
-
-	t.test("As copy", t => {
-		env.FOO = "bar";
-		try {
-			const { restoreEnv } = overrideEnv({ asCopy: true });
-			t.equal(process.env.FOO, "bar", "Should copy existing variables");
-			process.env.BAR = "elo";
-			t.equal(env.BAR, undefined, "Should not leak new vers on cached env");
-
-			restoreEnv();
 		} finally {
 			delete env.FOO;
 		}
