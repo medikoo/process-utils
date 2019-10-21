e2032a4346acc2cf3a7bce64fcfdc7c89b981f4e
Mariusz Nowak <mariusz@medikoo.com>
2019-10-21 15:40:35 +0200

    refactor(Override Env): Pass through options to createEnv

--- override-env.js
+++ override-env.js
@@ -1,14 +1,10 @@
 "use strict";
 
-const ensureIterable      = require("type/iterable/ensure")
-    , isPlainFunction     = require("type/plain-function/is")
+const isPlainFunction     = require("type/plain-function/is")
     , ensurePlainFunction = require("type/plain-function/ensure")
-    , ensureString        = require("type/string/ensure")
     , isObject            = require("type/object/is")
     , processCallback     = require("./lib/private/process-callback")
     , createEnv           = require("./create-env");
-
-const { hasOwnProperty: objHasOwnProperty } = Object.prototype;
 
 module.exports = (options = {}, callback = null) => {
 	ensurePlainFunction(callback, { isOptional: true });
@@ -18,27 +14,8 @@ module.exports = (options = {}, callback = null) => {
 	} else if (!isObject(options)) {
 		options = {};
 	}
-	if (
-		["asCopy", "whitelist", "variables"].reduce(
-			(count, optionName) => (options[optionName] ? count + 1 : count), 0
-		) > 1
-	) {
-		throw new Error("Only one of `asCopy`, `whitelist` or `variables` options can be provided");
-	}
-
-	const whitelist = ensureIterable(options.whitelist, {
-		isOptional: true,
-		ensureItem: ensureString,
-		errorMessage: "`whitelist` expected to be a string collection, got %v"
-	});
 	const original = process.env;
-	const replacement = createEnv({ variables: options.variables });
-	if (options.asCopy) Object.assign(replacement, original);
-	if (whitelist) {
-		for (const varName of whitelist) {
-			if (objHasOwnProperty.call(original, varName)) replacement[varName] = original[varName];
-		}
-	}
+	const replacement = createEnv(options);
 	process.env = replacement;
 	const restore = () => (process.env = original);
 
